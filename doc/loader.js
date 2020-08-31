5e2eed03a48876282f530cde2b50c462a8f4be9b
Robin Bron <finwo@pm.me>
2020-08-31 12:26:48 +0200

    Only remove tailing newlines from shebang and header, not from content

--- doc/loader.js
+++ doc/loader.js
@@ -4,7 +4,6 @@ const path = require('path');
 const optional = (out,id,name) => {
   try {
     const data = fs.readFileSync(path.resolve(__dirname,id,name)).toString().split('\r\n').join('\n').split('\n');
-    while(!data[data.length-1]) data.pop();
     out[name] = data;
   } catch(e) {
     // Do nothing
@@ -24,6 +23,11 @@ module.exports = id => {
   optional(out,id,'header');
   optional(out,id,'content');
 
+  // Remove tailing newlines from shebang + header
+  if (out.shebang.length) while(!out.shebang[out.shebang.length-1]) out.shebang.pop();
+  if (out.header.length) while(!out.header[out.header.length-1]) out.header.pop();
+
+  // Replace ID marker in header
   if (out.header) {
     out.header[0] = out.header[0].replace('{{id}}',id);
   }
